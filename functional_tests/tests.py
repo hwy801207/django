40382928ea056e61341678c02b8a6ead3642715f
hwy801207 <19130258@qq.com>
2016-05-15 15:27:54 +0800

    lalal

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,11 +2,27 @@
 
 from selenium import webdriver
 import unittest
+import sys
 from selenium.webdriver.common.keys import Keys
 #from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -21,7 +37,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name("h1").text
         self.assertIn('To-Do', header_text)
@@ -53,7 +69,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -74,7 +90,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertNotIn('make a fly', page_text)
 
     def test_layout_and_styling(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -83,7 +99,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
                 512,
                 delta = 5
                 )
-
-#if __name__ == '__main__':
-#    unittest.main(warnings='ignore')
-
