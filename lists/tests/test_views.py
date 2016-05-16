e46fb2c4f9385e816b61a073f617c1338773a073
hwy801207 <19130258@qq.com>
2016-05-16 19:34:03 +0800

    cctv2

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -73,16 +73,11 @@ class NewListTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post('/lists/new', data={'item_text', ""})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
 
-#     def test_redirect_after_post(self):
-# 
-#         response = self.client.post(
-#                 '/lists/new',
-#                 data = {'item_text': 'A new list item'}
-#                 )
-#         self.assertEqual(response.status_code, 302)
-#         self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-       #self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
 class NewItemTest(TestCase):
     def test_can_save_a_POST_request_to_an_existing_list(self):
