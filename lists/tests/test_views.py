ea8022a11c75a9a9259898a00e835d931b01ca0a
kaka.huang <kaka.huang@9icaishi.net>
2016-05-19 07:21:53 +0800

    一切ok

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -27,14 +27,8 @@ class HomePageTest(TestCase):
         
 
 
-class ListViewTest(TestCase):
-    def post_invalid_input(self):
-        list_ = List.objects.create()
-        return self.client.post(
-                                '/lists/%d/' %(list_.id,),
-                                data = {'text': ''}
-                                )
-
+class NewListTest(TestCase):
+        
     def test_displays_all_items(self):
         list_ = List.objects.create()
         Item.objects.create(text="itemey 1", list=list_)
@@ -86,12 +80,19 @@ class ListViewTest(TestCase):
         self.assertEqual(response.status_code, 200 )
         self.assertTemplateUsed(response, 'list.html')
         expected_error = escape("You can't have an empty list item")
-#         print(response.content.decode())
-#         print(response.status_code)
         self.assertContains(response, expected_error)
         
         
-class NewListTest(TestCase):
+
+    
+class ListViewTest(TestCase):
+    
+    def post_invalid_input(self):
+        list_ = List.objects.create()
+        return self.client.post(
+                                '/lists/%d/' %(list_.id,),
+                                data = {'text': ''}
+                                )
 
     def test_saving_a_post_request(self):
         self.client.post(
@@ -102,7 +103,7 @@ class NewListTest(TestCase):
         self.assertEqual(new_item.text, 'A new list item')
 
     def test_invalid_list_items_arent_saved(self):
-        self.client.post('/lists/new', data={'text': ''})
+        self.client.post('/lists/new', data={'id_text': ''})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
         
