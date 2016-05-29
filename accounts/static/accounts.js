72783e05ef51d2e8ac3981d6342e07af880dedb0
kaka.huang <kaka.huang@9icaishi.net>
2016-05-29 23:45:31 +0800

    nothing

---
+++ accounts/static/accounts.js
@@ -0,0 +1,9 @@
+var initialize = function(navigator) {
+	nagigator.id.request();
+
+};
+window.Superlists = {
+	Accounts : {
+		initialize : initialize
+	}
+};
--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,31 @@
 import sys
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 from django.contrib.auth import ( login as auth_login,
                                   logout as auth_logout
                                 )
 from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate
+from django.http.response import HttpResponse
+
 
 # Create your views here.
 
-def login(request):
-    print("login view", file=sys.stderr)
-    user = authenticate(assertion=request.POST['assertion'])
-    if user is not None:
-        auth_login(request, user)
-    return redirect('/')
+# def login(request):
+#     print('login view', file = sys.stderr)
+#     user = authenticate(assertion=request.POST['assertion'])
+#     if user is not None:
+#         auth_login(request, user)
+#     return redirect('/')
+# 
+# def logout(request):
+#     auth_logout(request)
+#     return redirect('/')
 
-def logout(request):
-    auth_logout(request)
-    return redirect('/')
+def persona_login(request):
+    user = authenticate(assertion = request.POST['assertion'])
+    if user:
+        login(request, user)
+    return HttpResponse('OK')
+
+
+    
